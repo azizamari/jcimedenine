a1c6ced6b88225989a4b1841f891c3c222adff9d
azizamari <azizamari2003@gmail.com>
2021-09-05 11:54:23 +0100

    Revert to old project

--- pdf/example.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var pdf = require('html-pdf');
-var html = fs.readFileSync('./template.html', 'utf8');
-var options = { format: 'A5' };
-
-pdf.create(html, options).toFile('./ex.pdf', function(err, res) {
-  if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
-});
